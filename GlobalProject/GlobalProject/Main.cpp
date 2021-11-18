000c066932fa650a74d463a2ad06516a09c34445
Dmitriy <trishkindima098@gmail.com>
2021-11-19 01:14:51 +0300

    Добавление параметров в main

--- GlobalProject/GlobalProject/Main.cpp
+++ GlobalProject/GlobalProject/Main.cpp
@@ -2,7 +2,22 @@
 using namespace std;
 int main()
 {
-	
+	int n;
+	cout << "1) Сумма" << endl;
+	cout << "1) Разница" << endl;
+	cout << "1) Умножение" << endl;
+	cout << "1) Деление" << endl;
+	cout << "Введите номер функции" << endl;
+	cin >> n;
+	switch (n)
+	{ 
+	case 1:
+	case 2:
+	case 3:
+	case 4:
+	default:
+		break;
+	}
 	system("pause");
 	return 0;
 }
